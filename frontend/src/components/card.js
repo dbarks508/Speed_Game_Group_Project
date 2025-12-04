52842519afe8adb1dd3c6d8fc224461657efde72
Michael Rasmussen <michaelrasmussen5@mail.weber.edu>
2025-12-04 14:46:02 -0700

    added Pile component (stack of cards)

--- frontend/src/components/card.js
+++ frontend/src/components/card.js
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const SUITS = ["clubs", "diamonds", "hearts", "spades"]
 
 function number_to_card_face(num){
@@ -9,6 +11,52 @@ function number_to_card_face(num){
 	else{
 		throw Error(`invalid number '${num}' for card`);
 	}
+}
+
+
+function PileComponent({cards, filterDrop, isDragable}){
+	var [cards, setCards] = useState(cards ?? []);
+
+	// TODO: enable an 'opaque' option where the backside is shown and we make a callback to get the next item (the callback should send a message to the server)
+
+	return (
+		<span
+			onDrop={(e) => {
+				let data = JSON.parse(e.dataTransfer.getData("json"));
+
+				if(data.suit != undefined && data.number != undefined){
+					cards.push({suit: data.suit, number: data.number});
+					setCards(cards.slice());
+				}
+			}}
+
+			onDragOver={(e) => {
+				let data = JSON.parse(e.dataTransfer.getData("json"));
+				if(data.suit == undefined || data.number == undefined) return;
+
+				if(filterDrop == undefined || filterDrop(data.suit, data.number)){
+					e.preventDefault();
+				}
+			}}
+		>
+		{cards.length == 0 ?
+			(<img className="card" onDragStart={(e) => e.preventDefault()} src="cards/blank.svg"/>):
+			(
+				<CardComponent
+					{...cards.at(-1)}
+					isDragable={isDragable}
+
+					onDrop={(e) => {
+						if(cards.length > 0){
+							cards.pop();
+							setCards(cards.slice());
+						}
+					}}
+				/>
+			)
+		}
+		</span>
+	);
 }
 
 function CardComponent({suit, number, isDragable, onDrop}){
@@ -40,4 +88,5 @@ function CardComponent({suit, number, isDragable, onDrop}){
 
 export{
 	CardComponent,
+	PileComponent,
 }
