2132dcb0f77a51800d1e2d3b099eb818a024941e
Dylan Barks <91198979+dbarks508@users.noreply.github.com>
2025-12-04 08:54:28 -0700

    welcome clean up

--- frontend/src/components/welcome.js
+++ frontend/src/components/welcome.js
@@ -6,106 +6,35 @@ export default function Welcome() {
   // vars and states
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState("");
-  const [form, setForm] = useState({
-    player: "",
-    word: "",
-    random: false,
-  });
-
-  // form update helper
-  function updateForm(jsonObj) {
-    return setForm((prevJsonObj) => {
-      return { ...prevJsonObj, ...jsonObj };
-    });
-  }
+  cosnt[(playerName, setPlayerName)] - useState("");
 
   // function triggered by user submit
   async function onSubmit(e) {
     e.preventDefault();
 
-    let data;
-    let playerData = {};
-
-    if (form.random) {
-      // player object from form data without chosen word
-      playerData = {
-        player: form.player,
-      };
-
-      // send player data to back end for session use
-      const responce = await fetch("http://localhost:4000/init-game-random", {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(playerData),
-      });
-
-      // get and handle responce
-      data = await responce.json();
-    } else {
-      // player object from form data
-      playerData = {
-        player: form.player,
-        word: form.word,
-      };
-
-      // send player data to back end for session use
-      const responce = await fetch("http://localhost:4000/init-game", {
-        method: "POST",
-        credentials: "include",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(playerData),
-      });
-
-      // get and handle responce
-      data = await responce.json();
+    if (!playerName) {
+      return;
     }
 
-    // confirm session set correctly
-    if (data.message === "session set") {
-      console.log("Data recieved from route /init-game: ", data);
-      navigate("/waiting");
-    } else {
-      setErrorMessage("Problem setting session data.");
-    }
+    navigate("/waiting", { state: { playerName: playerName } });
   }
 
   // display
   return (
     <div className="body">
-      <h1>Welcome to hangman!</h1>
-      <h3>Enter your name and a word for your opponent to guess</h3>
+      <h1>Welcome to the Game of Speed</h1>
+      <h3>Enter your player name to joing the waiting room.</h3>
       <form onSubmit={onSubmit}>
         <div>
-          <label>Player name: </label>
+          <label>Name: </label>
           <input
             type="text"
             id="player-name"
-            value={form.player}
-            onChange={(e) => updateForm({ player: e.target.value })}
+            value={playerName}
+            onChange={(e) => setPlayerName(e.target.value)}
             required
           />
         </div>
-        <div>
-          <label>Word: </label>
-          <input
-            type="text"
-            id="word"
-            placeholder="non-random type here"
-            value={form.word}
-            onChange={(e) => updateForm({ word: e.target.value })}
-          />
-        </div>
-        <div>
-          <label>Random word: </label>
-          <input
-            type="checkbox"
-            id="random"
-            checked={form.random}
-            onChange={(e) => updateForm({ random: e.target.checked })}
-          />
-        </div>
-        <br />
         <div>
           <input type="submit" value="Submit"></input>
         </div>
